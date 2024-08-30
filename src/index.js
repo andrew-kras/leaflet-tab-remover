42ad322b31152f6005bbcf45b5682000580cd152
andrew-kras <andrewkrasj@mail.ru>
2024-08-30 20:50:47 +0300

    Bump version to 1.0.1

--- src/index.js
+++ src/index.js
@@ -15,19 +15,21 @@ function addLeafletStyles() {
     document.head.appendChild(style);
 }
 
+function removeTabIndex() {
+    const mapElement = document.querySelector('.leaflet-container');
+    if (mapElement) {
+        mapElement.setAttribute('tabindex', '-1');
+        const elements = mapElement.querySelectorAll('.leaflet-interactive');
+        elements.forEach(element => {
+            element.setAttribute('tabindex', '-1');
+        });
+    }
+}
+
 export default {
     install(app) {
         addLeafletStyles();
 
-        app.config.globalProperties.$removeLeafletTabIndex = function () {
-            const mapElement = document.querySelector('.leaflet-container');
-            if (mapElement) {
-                mapElement.setAttribute('tabindex', '-1');
-                const elements = mapElement.querySelectorAll('.leaflet-interactive');
-                elements.forEach(element => {
-                    element.setAttribute('tabindex', '-1');
-                });
-            }
-        };
+        app.config.globalProperties.$removeLeafletTabIndex = removeTabIndex;
     }
 };
