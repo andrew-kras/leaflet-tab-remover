d70beb30958063b62843f1bdff6cf5910a343507
andrew-kras <andrewkrasj@mail.ru>
2024-08-30 20:33:02 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log('Happy developing ✨')
+function addLeafletStyles() {
+    const style = document.createElement('style');
+    style.textContent = `
+    .leaflet-interactive {
+      outline: none !important;
+      box-shadow: none !important;
+      -moz-outline-style: none !important;
+    }
+    .leaflet-container {
+      outline: none !important;
+      box-shadow: none !important;
+      -moz-outline-style: none !important;
+    }
+  `;
+    document.head.appendChild(style);
+}
+
+export default {
+    install(app) {
+        addLeafletStyles();
+
+        app.config.globalProperties.$removeLeafletTabIndex = function () {
+            const mapElement = document.querySelector('.leaflet-container');
+            if (mapElement) {
+                mapElement.setAttribute('tabindex', '-1');
+                const elements = mapElement.querySelectorAll('.leaflet-interactive');
+                elements.forEach(element => {
+                    element.setAttribute('tabindex', '-1');
+                });
+            }
+        };
+    }
+};
